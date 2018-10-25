caa05b4a0445a991282364e66cb3377da8dfdacd
Carter Baldwin <carterbaldwin@gmail.com>
2018-10-25 17:04:18 -0400

    so close

--- client/src/pages/Clothes/Clothes.js
+++ client/src/pages/Clothes/Clothes.js
@@ -8,12 +8,26 @@ import { Input, TextArea, FormBtn } from "../../components/Form";
 
 import AddBtn from "../../components/AddBtn";
 import Mannequin from "../../components/images/2dmannequin.png";
+import Dress from "../../components/images/dress.png";
+import Hat from "../../components/images/hat.png";
+import Pants from "../../components/images/pants.png";
+import Shirt from "../../components/images/shirt.png";
+import Shoes from "../../components/images/shoes.png";
+
+import "./Clothes.css";
+
 
 
 
 class Clothes extends Component {
   state = {
-    clothes: []
+    clothes: [],
+    bodyWear: "",
+    hat: "",
+    shoes: "",
+    pants: "",
+    shirt: "",
+
   };
 
   componentDidMount() {
@@ -26,25 +40,61 @@ class Clothes extends Component {
       .catch(err => console.log(err));
   };
 
+ 
+
   updateMannequin = (type) => {
+
+    if (type==="full body" || type==="dress" || type==="jumpsuit"){
+      this.setState({bodyWear: Dress})
+      this.setState({shirt: ""})
+      this.setState({pants: ""})      
+    }
+    if (type==="shoes"){
+      this.setState({shoes: Shoes})
+    }
+    if (type==="hat"){
+      this.setState({hat: Hat})
+    }
+    if (type==="bottom" || type==="pants"){
+      this.setState({pants: Pants})
+      this.setState({bodyWear: ""})
+
+    }
+    if (type==="top" || type==="shirt"){
+      this.setState({shirt: Shirt})
+      this.setState({bodyWear: ""})
+
+    }
     console.log(type)
+
+  }
+
+  removeItem = (type) => {
+
+    if (type==="full body" || type==="dress" || type==="jumpsuit"){
+      this.setState({bodyWear: ""})
+    }
+    if (type==="shoes"){
+      this.setState({shoes: ""})
+    }
+    if (type==="hat"){
+      this.setState({hat: ""})
+    }
+    if (type==="bottom" || type==="pants"){
+      this.setState({pants: ""})
+    }
+    if (type==="top" || type==="shirt"){
+      this.setState({shirt: ""})
+
+    }
+
   }
 
   render() {
     return (
       <Container fluid>
         <Row>
-          {/* <Col size="md-6">
-            <Jumbotron>
-              <h1>What Clothing Should I Wear?</h1>
-            </Jumbotron>
-            <form>
-              <Input name="type" placeholder="type of clothing" />
-              <Input name="name" placeholder="name of clothing" />
-              <TextArea name="link" placeholder="link to clothing" />
-              <FormBtn>Submit Clothing</FormBtn>
-            </form>
-          </Col> */}
+         
           <Col size="md-6 sm-12">
             <Jumbotron>
               <h1>Clothes On My List</h1>
@@ -57,22 +107,22 @@ class Clothes extends Component {
             {this.state.clothes.length ? (
               <List>
                 {this.state.clothes.map(clothes => (
-                  <ListItem key={clothes._id}>
+                  <ListItem key={clothes.type}>
                     {/* <a href={"/clothes/" + clothes._id}> */}
                     
                     <ul onClick ={ () => this.updateMannequin(clothes.type)}><AddBtn /></ul>
 
                     <a href={clothes.link}>
                       <strong>
-                        <ul>
+                        <ul><h5>
                         {clothes.name}
-                        </ul>
+                        </h5></ul>
                         {/* <ul>
                         <a href={clothes.link}/>
                         </ul> */}
                       </strong>
                     </a>
-                    <DeleteBtn />
+                    <ul onClick ={ () => this.removeItem(clothes.type)}><DeleteBtn /></ul>
                   </ListItem>
                 ))}   
               </List>
@@ -86,11 +136,21 @@ class Clothes extends Component {
               <h1>What Clothing Should I Wear?</h1>
             </Jumbotron>
             <form>
-              <ul> <img src= {Mannequin} /></ul>
-              {/* <Input name="type" placeholder="type of clothing" />
-              <Input name="name" placeholder="name of clothing" />
-              <TextArea name="link" placeholder="link to clothing" />
-              <FormBtn>Submit Clothing</FormBtn> */}
+              {/* <ul> <img src= {Mannequin} /></ul> */}
+
+              <div class="parent">
+                <img class="image1" src={Mannequin} />
+                {/* {this.state.type}? */}
+                <img class="image2" src={this.state.bodyWear} />
+                <img class="image2" src={this.state.hat} />
+                <img class="image2" src={this.state.shoes} />
+                
+                <img class="image2" src={this.state.pants} />
+                <img class="image2" src={this.state.shirt} />
+
+
+              </div>
+              
             </form>
           </Col>
         </Row>
